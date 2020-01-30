f7092c3c0c3a129ddc583f848b00483950177e28
Lisa <lisa.michael@hotmail.com>
2020-01-30 11:54:02 -0500

    index.js. first js file

---
+++ model/index.js
@@ -0,0 +1,9 @@
+// Database configuration
+var databaseUrl = "kukukwes";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
