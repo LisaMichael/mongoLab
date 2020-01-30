15ed2a54292e97a1419ed6c7f2ce8ee07b8db0ec
Lisa <lisa.michael@hotmail.com>
2020-01-30 13:46:46 -0500

    still working on my mongoose database

---
+++ app/model/kukukwes.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+// Database configuration
+var databaseUrl = "kukukwes";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
