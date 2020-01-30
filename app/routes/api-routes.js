f2af99016e86bdd9783637bd6b36883fddb63b21
Lisa <lisa.michael@hotmail.com>
2020-01-30 12:56:12 -0500

    added  api-route.js but no code

---
+++ app/routes/api-routes.js
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models/");
+let express = require("express");
+let router = express.Router();
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+
+    // need api-route code here
+    
+
+   
+    
+};
