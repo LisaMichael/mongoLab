53807d0c10a2f2a1c461908246fbe792e447109a
Lisa <lisa.michael@hotmail.com>
2020-02-01 00:04:36 -0500

    added micmac news to server.js

--- server.js
+++ server.js
@@ -27,19 +27,20 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/micmac", { useNewUrlParser: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.mmnn.ca/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
+    // console.log(response);
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2.post-title").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
